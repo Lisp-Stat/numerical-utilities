48258edd28065c7b647504080195212fc01d5945
RALPH <ralph@num-utils.dev>
2026-03-03 16:46:43 +0000

    feat(US-012): Write trig unary tests (etan, easin, eacos, e1atan, esinh, ecosh, etanh, easinh, eacosh, eatanh)

--- tests/elementwise.lisp
+++ tests/elementwise.lisp
@@ -44,6 +44,133 @@
     (assert-true (num= #(1.0) (elog #(10) 10)))
     (assert-true (num= a (eexp (elog a))))))
 
+;;; -----------------------------------------------------------------------
+;;; US-012: Tier 1 trig unary tests
+;;; -----------------------------------------------------------------------
+
+(defsuite elementwise-trig (elementwise))
+
+(deftest etan-scalar (elementwise-trig)
+  "etan on a scalar equals tan."
+  (assert-true (num= (tan 1.0d0) (etan 1.0d0))))
+
+(deftest etan-array (elementwise-trig)
+  "etan maps tan over each element of an array."
+  (let* ((x #(0.0d0 0.5d0 1.0d0))
+         (r (etan x)))
+    (assert-true (num= (tan 0.0d0) (aref r 0)))
+    (assert-true (num= (tan 0.5d0) (aref r 1)))
+    (assert-true (num= (tan 1.0d0) (aref r 2)))))
+
+(deftest easin-scalar (elementwise-trig)
+  "easin on a scalar equals asin."
+  (assert-true (num= (asin 0.5d0) (easin 0.5d0))))
+
+(deftest easin-array (elementwise-trig)
+  "easin maps asin over each element of an array (domain [-1, 1])."
+  (let* ((x #(0.0d0 0.5d0 1.0d0))
+         (r (easin x)))
+    (assert-true (num= (asin 0.0d0) (aref r 0)))
+    (assert-true (num= (asin 0.5d0) (aref r 1)))
+    (assert-true (num= (asin 1.0d0) (aref r 2)))))
+
+(deftest eacos-scalar (elementwise-trig)
+  "eacos on a scalar equals acos."
+  (assert-true (num= (acos 0.5d0) (eacos 0.5d0))))
+
+(deftest eacos-array (elementwise-trig)
+  "eacos maps acos over each element of an array (domain [-1, 1])."
+  (let* ((x #(0.0d0 0.5d0 1.0d0))
+         (r (eacos x)))
+    (assert-true (num= (acos 0.0d0) (aref r 0)))
+    (assert-true (num= (acos 0.5d0) (aref r 1)))
+    (assert-true (num= (acos 1.0d0) (aref r 2)))))
+
+(deftest e1atan-scalar (elementwise-trig)
+  "e1atan on a scalar equals atan (1-arg form)."
+  (assert-true (num= (atan 1.0d0) (e1atan 1.0d0))))
+
+(deftest e1atan-array (elementwise-trig)
+  "e1atan maps atan over each element of an array."
+  (let* ((x #(0.0d0 1.0d0 2.0d0))
+         (r (e1atan x)))
+    (assert-true (num= (atan 0.0d0) (aref r 0)))
+    (assert-true (num= (atan 1.0d0) (aref r 1)))
+    (assert-true (num= (atan 2.0d0) (aref r 2)))))
+
+(deftest esinh-scalar (elementwise-trig)
+  "esinh on a scalar equals sinh."
+  (assert-true (num= (sinh 1.0d0) (esinh 1.0d0))))
+
+(deftest esinh-array (elementwise-trig)
+  "esinh maps sinh over each element of an array."
+  (let* ((x #(0.0d0 1.0d0 2.0d0))
+         (r (esinh x)))
+    (assert-true (num= (sinh 0.0d0) (aref r 0)))
+    (assert-true (num= (sinh 1.0d0) (aref r 1)))
+    (assert-true (num= (sinh 2.0d0) (aref r 2)))))
+
+(deftest ecosh-scalar (elementwise-trig)
+  "ecosh on a scalar equals cosh."
+  (assert-true (num= (cosh 1.0d0) (ecosh 1.0d0))))
+
+(deftest ecosh-array (elementwise-trig)
+  "ecosh maps cosh over each element of an array."
+  (let* ((x #(0.0d0 1.0d0 2.0d0))
+         (r (ecosh x)))
+    (assert-true (num= (cosh 0.0d0) (aref r 0)))
+    (assert-true (num= (cosh 1.0d0) (aref r 1)))
+    (assert-true (num= (cosh 2.0d0) (aref r 2)))))
+
+(deftest etanh-scalar (elementwise-trig)
+  "etanh on a scalar equals tanh."
+  (assert-true (num= (tanh 1.0d0) (etanh 1.0d0))))
+
+(deftest etanh-array (elementwise-trig)
+  "etanh maps tanh over each element of an array."
+  (let* ((x #(0.0d0 1.0d0 2.0d0))
+         (r (etanh x)))
+    (assert-true (num= (tanh 0.0d0) (aref r 0)))
+    (assert-true (num= (tanh 1.0d0) (aref r 1)))
+    (assert-true (num= (tanh 2.0d0) (aref r 2)))))
+
+(deftest easinh-scalar (elementwise-trig)
+  "easinh on a scalar equals asinh."
+  (assert-true (num= (asinh 1.0d0) (easinh 1.0d0))))
+
+(deftest easinh-array (elementwise-trig)
+  "easinh maps asinh over each element of an array."
+  (let* ((x #(0.0d0 1.0d0 2.0d0))
+         (r (easinh x)))
+    (assert-true (num= (asinh 0.0d0) (aref r 0)))
+    (assert-true (num= (asinh 1.0d0) (aref r 1)))
+    (assert-true (num= (asinh 2.0d0) (aref r 2)))))
+
+(deftest eacosh-scalar (elementwise-trig)
+  "eacosh on a scalar equals acosh (domain: x >= 1)."
+  (assert-true (num= (acosh 2.0d0) (eacosh 2.0d0))))
+
+(deftest eacosh-array (elementwise-trig)
+  "eacosh maps acosh over each element of an array (values >= 1)."
+  (let* ((x #(1.0d0 2.0d0 3.0d0))
+         (r (eacosh x)))
+    (assert-true (num= (acosh 1.0d0) (aref r 0)))
+    (assert-true (num= (acosh 2.0d0) (aref r 1)))
+    (assert-true (num= (acosh 3.0d0) (aref r 2)))))
+
+(deftest eatanh-scalar (elementwise-trig)
+  "eatanh on a scalar equals atanh (domain: |x| < 1)."
+  (assert-true (num= (atanh 0.5d0) (eatanh 0.5d0))))
+
+(deftest eatanh-array (elementwise-trig)
+  "eatanh maps atanh over each element of an array (values in (-1, 1))."
+  (let* ((x #(0.0d0 0.5d0 -0.5d0))
+         (r (eatanh x)))
+    (assert-true (num= (atanh  0.0d0) (aref r 0)))
+    (assert-true (num= (atanh  0.5d0) (aref r 1)))
+    (assert-true (num= (atanh -0.5d0) (aref r 2)))))
+
+
 ;;; Commented out by Papp. Should these be in array-operations?
 ;; (deftest (elementwise-tests)
 ;;   stack-tests
